1f3c6d6c53950344421334ce840153837746ccd8
Shah Solayman Sinha <md.sinha.p@gmail.com>
2023-08-25 17:57:48 +0600

    api created for admin panel and price section api created

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require("mongodb");
+const bodyParser = require("body-parser");
 
 require("dotenv").config();
 
 // middleware
 app.use(cors());
 app.use(express.json());
+app.use(bodyParser.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ug5vbmv.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -30,6 +32,8 @@ async function run() {
 		const individualPackagesCollection = client.db("Rishita-Driving-School").collection("Individual Packages");
 		const standardPackagesCollection = client.db("Rishita-Driving-School").collection("Standard Packages");
 
+		const stateDB = client.db("RDS-State-Wise-Price");
+
 		app.get("/basic-packages", async (req, res) => {
 			const cursor = basicPackagesCollection.find();
 			const result = await cursor.toArray();
@@ -46,6 +50,84 @@ async function run() {
 			const cursor = standardPackagesCollection.find();
 			const result = await cursor.toArray();
 			res.send(result);
+		});
+
+		app.get("/all-states", async (req, res) => {
+			const collectionNames = await stateDB.listCollections().toArray();
+			const names = collectionNames.map(collection => collection.name);
+			res.send(names);
+		});
+
+		app.get("/get-state-data/:stateName", async (req, res) => {
+			const stateName = req.params.stateName;
+			const stateData = await stateDB.collection(stateName).find().toArray();
+			res.send(stateData);
+		});
+
+		app.get("/state/price/:stateName", async (req, res) => {
+			const stateName = req.params.stateName;
+			const stateData = await stateDB.collection(stateName).find().toArray();
+			res.send(stateData);
+		});
+
+		app.get("/state/price/:stateName/basic", async (req, res) => {
+			const stateName = req.params.stateName;
+			const stateData = await stateDB
+				.collection(stateName)
+				.find({ packageCategory: { $regex: "basic", $options: "i" } })
+				.toArray();
+			res.send(stateData);
+		});
+		app.get("/state/price/:stateName/standard", async (req, res) => {
+			const stateName = req.params.stateName;
+			const stateData = await stateDB
+				.collection(stateName)
+				.find({ packageCategory: { $regex: "standard", $options: "i" } })
+				.toArray();
+			res.send(stateData);
+		});
+		app.get("/state/price/:stateName/individual", async (req, res) => {
+			const stateName = req.params.stateName;
+			const stateData = await stateDB
+				.collection(stateName)
+				.find({ packageCategory: { $regex: "individual", $options: "i" } })
+				.toArray();
+			res.send(stateData);
+		});
+
+		app.post("/create-state-wise-price", (req, res) => {
+			try {
+				const packageData = req.body;
+				console.log(packageData);
+
+				const collection = stateDB.collection(packageData.stateName);
+
+				collection.insertOne(packageData, (err, result) => {
+					if (err) {
+						console.error("Error inserting document:", err);
+						res.status(500).json({ error: "An error occurred" });
+					} else {
+						res.status(201).json({ message: "Package created successfully" });
+					}
+				});
+			} catch (error) {
+				console.error("Error:", error);
+				res.status(500).json({ error: "An error occurred" });
+			}
+		});
+
+		app.post("/update-state-wise-price", async (req, res) => {
+			try {
+				const packageData = req.body;
+				console.log(packageData);
+
+				const collection = stateDB.collection(packageData.stateName);
+
+				const result = await collection.replaceOne({ packageName: packageData.packageName }, packageData);
+			} catch (error) {
+				console.error("Error:", error);
+				res.status(500).json({ error: "An error occurred" });
+			}
 		});
 
 		// Send a ping to confirm a successful connection
